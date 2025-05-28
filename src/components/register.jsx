78f0a1d17bc6344b4294413592aa232dc1c90ce9
Utkarsh Saxena <utkarshsaxena851@gmail.com>
2025-05-28 18:16:29 +0530

    minoor

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -65,7 +65,7 @@ function Register() {
      const  mainContract= new ethers.Contract(add,ABI,signer);
              // setContract(mainContract);
     try{
-      const response=await axios.post('ngo-backend-production.up.railway.app/registerUser',formData,{
+      const response=await axios.post('https://ngo-backend-production.up.railway.app/registerUser',formData,{
         headers:{
           'Content-Type':'application/json',
         },
